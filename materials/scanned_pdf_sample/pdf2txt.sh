0907ab2b3702a2c891a47672a4b14d0c896a169e
Alex Flückiger <alex.flueckiger@gmail.com>
2020-04-23 13:51:26 +0200

    minor changes data samples

--- materials/scanned_pdf_sample/pdf2txt.sh
+++ materials/scanned_pdf_sample/pdf2txt.sh
@@ -5,21 +5,26 @@
 # replace spaces with underscores in filenames
 for f in *\ *; do mv "$f" "${f// /_}"; done
 
+echo "Start OCR process. Please wait."
 
 # convert pdf-files to txt-files
 for FILEPATH in *.pdf; do 
+
+    echo "Perform OCR on $FILEPATH"
 
 	# convert pdf to image
     convert -density 300 $FILEPATH -depth 8 -strip \
     -background white -alpha off temp.tiff
     
-    # define output name
-    OUTFILE=${FILEPATH%.docx} 
+    # define output name (remove .pdf, .txt suffix will be added by tesseract)
+    OUTFILE=${FILEPATH%.pdf} 
     
-    # perform OCR on the tiff image
+    # perform OCR on the tiff image (change language if needed, deu=German)
     tesseract -l deu temp.tiff $OUTFILE
     
     # remove the intermediate tiff image
     rm temp.tiff
 
 done
+
+echo "OCR process finished successfully."
